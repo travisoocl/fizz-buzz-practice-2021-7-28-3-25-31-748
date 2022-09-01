dc04c2f7f2f553cc9744a1f79e42e33c5d86ff07
munoztr <munoztr@oocl.com>
2022-09-01 14:12:13 +0800

    feat: output fizz if number can be divided by 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,11 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public static String assess(int number) {
-        if(number%3==0)
-        {
+        if (number % 3 == 0) {
             return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
         }
         return String.valueOf(number);
     }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -8,20 +8,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void should_print_number_when_assess_number_given_1(){
+    public void should_print_number_when_assess_number_given_1() {
 
         String expected = String.valueOf(1);
         String actual = FizzBuzz.assess(1);
 
-        assertEquals(expected,actual );
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void should_print_fizz_when_assess_number_given_3(){
+    public void should_print_fizz_when_assess_number_given_3() {
 
         String expected = "Fizz";
         String actual = FizzBuzz.assess(3);
 
-        assertEquals(expected,actual );
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_print_buzz_when_assess_number_given_5() {
+
+        String expected = "Buzz";
+        String actual = FizzBuzz.assess(5);
+
+        assertEquals(expected, actual);
     }
 }
