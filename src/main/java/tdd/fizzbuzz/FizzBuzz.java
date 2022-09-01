9db868b6c6b386d524d3ba5da3432e904ac042b2
munoztr <munoztr@oocl.com>
2022-09-01 14:06:44 +0800

    feat: output number as string

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,7 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static String assess(int number) {
+        return String.valueOf(number);
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_print_number_when_assess_number_given_1(){
+
+    String expected = String.valueOf(1);
+    String actual = FizzBuzz.assess(1);
+
+    assertEquals(expected,actual );
+    }
 }
